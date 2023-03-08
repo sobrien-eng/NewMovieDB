e28421db1d26c5ba1ca02e4cc1bc64fe7a0a254d
sobrien-eng <sobrien@student.neumont.edu>
2023-03-07 19:24:33 -0700

    update

--- moviedb/server/controllers/revRat/revRat.controller.js
+++ moviedb/server/controllers/revRat/revRat.controller.js
@@ -2,8 +2,8 @@ import { db } from "../../db.config.js";
 
 export const addReview = (req, res) => {
 //NEED MOVIE ID
-    db.query(q, [req.body.review, req.body.rating], (err, data) => {
-        const q = "INSERT INTO reviewRating (`review`,`rating`) VALUES (?)";
+    db.query(q, [req.body.review, req.body.rating, req.body.movieId], (err, data) => {
+        const q = "INSERT INTO reviewRating (`review`,`rating`, `movieId`) VALUES (?)";
         const values = [req.body.review, req.body.rating];
 
         db.query(q, [values], (err, data) => {
@@ -22,7 +22,7 @@ export const getRevRats = (req, res) => {
 
     const q = "SELECT * FROM reviewRating";
 
-    db.query(q, [req.body.review], [req.body.rating], (err, data) => {
+    db.query(q, [req.body.review], [req.body.rating], [req.body.movieId], (err, data) => {
         console.log(req.body.review);
         console.log(req.body.rating);
         //NEED MOVIE ID
@@ -38,7 +38,26 @@ export const getRevRats = (req, res) => {
     });
     console.log("ouch2");
 };
-
+//SELECT * FROM USERS U
+//JOIN REVIEWS R 
+//U.ID = R. UID
+//WHERE U.USERNAME =.....
 export const deleteReview = async (req, res) => {
+    const q = "SELECT * FROM reviewRating WHERE ";
+
+    db.query(q, [req.body.review], [req.body.rating], [req.body.movieId], (err, data) => {
+        console.log(req.body.review);
+        console.log(req.body.rating);
+        //NEED MOVIE ID
+        if (err) {
+            console.log("select");
+            console.log(JSON.stringify("error"+ err));
+            return res.status(500).json(err);
+        }
+        if (data.length === 0) {
+            return res.status(404).json("No data found!");
+        }
+
+    });
     console.log("ouch2");
 };
